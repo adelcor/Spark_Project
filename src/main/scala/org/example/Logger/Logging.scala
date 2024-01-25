52bbc1f1789f1a488719c8fc55be16ff72866e52
adelcor <adelcorralm@gmail.com>
2024-01-25 09:28:56 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/scala/org/example/App.scala
    #	src/main/scala/org/example/CSVWriter.scala
    #	src/main/scala/org/example/Logging.scala

--- src/main/scala/org/example/Logger/Logging.scala
+++ src/main/scala/org/example/Logger/Logging.scala
@@ -5,13 +5,19 @@ import org.apache.logging.log4j.{LogManager, Logger}
 /**
  * Trait that provides logging functionality.
  *
- * This trait can be mixed into any class that requires logging capabilities.
- * It provides a `logger` val which is an instance of `Logger` from the Log4j library.
- * The logger is initialized with the class name of the class into which this trait is mixed.
+ * This trait can be mixed into any class to provide logging capabilities. It offers a `logger` val,
+ * which is an instance of `Logger` from the Log4j library, initialized with the class name.
+ *
+ * Example Usage:
+ * class MyClass extends Logging {
+ *   logger.info("Logging info from MyClass")
+ * }
  */
 trait Logging {
   // Logger instance specific to the class that mixes in this trait.
   // The logger is obtained using the `LogManager` from the Log4j library.
   // `getClass` ensures that the logger is tagged with the correct class name.
   val logger: Logger = LogManager.getLogger(getClass)
+
+
 }
